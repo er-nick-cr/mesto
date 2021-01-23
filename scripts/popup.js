2b3c54b06d6ab509b48c9a65dc008cf1c0ba180d
er-nick-cr <nikiteremeev2010@gmail.com>
2021-01-23 12:30:19 +0700

    исправить ошибки

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,12 +1,10 @@
 let popup = document.querySelector('.popup');
 let profileEditButton = document.querySelector('.profile__edit-button');
-let formContainer = document.querySelector('.popup__container');
-let formInputContainer = formContainer.querySelector('.popup__input-container');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_description');
 let profileAuthor = document.querySelector('.profile__author');
 let profileDescription = document.querySelector('.profile__description');
-let closeButton = formContainer.querySelector('.popup__close-icon');
+let closeButton = document.querySelector('.popup__close-icon');
 let form = document.querySelector('.popup__container');
 
 function popupOpened () {
@@ -15,17 +13,18 @@ function popupOpened () {
     jobInput.value = profileDescription.textContent;
 };
 
+
+function popupClose () {
+    popup.classList.remove('popup_opened');
+}
+
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
     profileAuthor.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+    popupClose ();
 }
 
-function popupClose (a) {
-    a.preventDefault();
-    popup.classList.remove('popup_opened');
-}
 profileEditButton.addEventListener('click', popupOpened) 
 
 form.addEventListener('submit', handleFormSubmit); 
